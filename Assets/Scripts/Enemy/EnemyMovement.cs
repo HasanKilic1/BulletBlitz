ea9a5742c8ea5dba49c28d63cefd52b1bf25d7be
Hasan Kılıc <hasan.58125@gmail.com>
2024-12-10 00:33:53 +0300

    Folder structure

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,6 +4,7 @@ public class EnemyMovement : MonoBehaviour
 {
     private Player player;
     [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float playerDetectionRange = 1.5f;
     void Start()
     {
         player = FindFirstObjectByType<Player>();
@@ -16,9 +17,24 @@ public class EnemyMovement : MonoBehaviour
 
     void Update()
     {
+        FollowPlayer();
+        TryAttack();
+    }
+
+    private void FollowPlayer()
+    {
         Vector3 direction = player.transform.position - transform.position;
         Vector3 targetPosition = transform.position + moveSpeed * Time.deltaTime * direction.normalized;
 
         transform.position = targetPosition;
+    }
+
+    private void TryAttack()
+    {
+        float distance = Vector2.Distance(transform.position , player.transform.position);
+        if(distance < playerDetectionRange)
+        {
+            Destroy(gameObject);
+        }
     }
 }
